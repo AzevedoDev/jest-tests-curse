b6f996475e0957d4414469abc5d0e15ac6c460f5
Pedro Azevedo <theazevedo@outlook.com>
2021-01-07 21:42:25 -0300

    Finalizando introdução

--- projeto1/src/lib/calculator.spec.js
+++ projeto1/src/lib/calculator.spec.js
@@ -1,19 +1,19 @@
-const { sum } = require("./calculator");
+import { sum } from './calculator';
 
-it("should sum 2 and 2 and the result must be 4", () => {
+it('should sum 2 and 2 and the result must be 4', () => {
   expect(sum(2, 2)).toBe(4);
 });
 
 it(`should sum 2 and 2 even 
     if on of then is a string and the 
     result must be 4`, () => {
-  expect(sum("2", "2")).toBe(4);
+  expect(sum('2', '2')).toBe(4);
 });
 
 it(`should throw an erro if what is provided
     to the method cannot be summed`, () => {
   expect(() => {
-    sum("", "2");
+    sum('', '2');
   }).toThrowError();
 
   expect(() => {
